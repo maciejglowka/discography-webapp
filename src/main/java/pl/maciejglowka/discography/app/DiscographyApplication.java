3fe91cc027415b53e8baaf99244aa10e8a8d609b
Maciej Glowka <maciejglowka.pl@gmail.com>
2020-06-26 00:50:29 +0200

    initial springboot + react commit

---
+++ src/main/java/pl/maciejglowka/discography/app/DiscographyApplication.java
@@ -0,0 +1,13 @@
+package pl.maciejglowka.discography.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscographyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscographyApplication.class, args);
+    }
+
+}
